b7c96984cc1fb7d2a3649bebc87a9cfb74238565
Jan Wasilewski <janwasilewski144@op.pl>
2021-05-13 13:19:01 +0200

    changes to solution_data

--- solution_data.py
+++ solution_data.py
@@ -7,6 +7,9 @@ class solution_instance:
 
 
 def calculate_fitness(solution)
-    {
-
-    }
+    result = 0
+    n = len(solution.order)
+    for x in solution.order
+        result += (n*x)
+        n = n - 1
+    return result
