1d3caa4ded3cd51e81e39c44df90e28d5d91e1ed
Jan Wasilewski <janwasilewski144@op.pl>
2021-05-13 13:23:05 +0200

    changes to solution_data

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
+import solution_data
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+test = solution_data()
+test.order = [4, 2, 6]
 
+test.fitness = calculate_fitness(test)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(test.fitness)
