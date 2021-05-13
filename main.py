9517f5d933f84872fc9c4e211acd92d40495a6a8
Waldemar <waldek99zielinski@gmail.com>
2021-05-13 19:47:42 +0200

    evolution: crossover first draft

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from solution_data import *
+from evolution_crossover import *
+# order = [4, 2, 6]
+# test = SolutionInstance(order)
+# 
+# 
+# print(test.fitness)
 
-order = [4, 2, 6]
-test = SolutionInstance(order)
+order1 = [2, 4, 9, 21, 41, 7, 4, 2]
+solution1 = SolutionInstance(order1)
+order2 = [21, 4, 7, 4, 41, 9, 2, 2]
+solution2 = SolutionInstance(order2)
 
-
-print(test.fitness)
+order_crossover(solution1, solution2)
