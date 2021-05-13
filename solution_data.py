915d8f39ee1e2fae40669abd8c604b49f4178da7
Jan Wasilewski <janwasilewski144@op.pl>
2021-05-13 13:11:50 +0200

    changes to solution_data

--- solution_data.py
+++ solution_data.py
@@ -1,1 +1,12 @@
 # For implementing solution data structures and fitness function
+import math
+
+class solution_instance:
+    order = []
+    fitness = math.inf
+
+
+def calculate_fitness(solution)
+    {
+
+    }
