c2409df9f08c85d34ebc33019e72915340554254
Waldemar <waldek99zielinski@gmail.com>
2021-05-16 17:51:42 +0200

    crossover code cleanup

--- evolution_crossover.py
+++ evolution_crossover.py
@@ -7,10 +7,10 @@ def order_crossover(solution_instance1, solution_instance2):
     random.seed(101)
     # order for each solution has the same length
     matchingSectionIndex = random.randrange(len(solution_instance1.order))
-    print("matchingSectionIndex: ", matchingSectionIndex)
+
     # unchanged part of solution
     crossedSolutionOrder = solution_instance1.order[:matchingSectionIndex]
-    print("crossed soluttion part: ", crossedSolutionOrder)
+
     missingSolutionPartOrder = []
     for s in range(matchingSectionIndex, len(solution_instance1.order)):
         index = solution_instance2.order.index(solution_instance1.order[s])
@@ -18,15 +18,12 @@ def order_crossover(solution_instance1, solution_instance2):
         # populate array with pair index, time
         missingSolutionPartOrder.append((index, solution_instance1.order[s]))
 
-    pair = [(1, 2)]
-    print(missingSolutionPartOrder)
     missingSolutionPartOrder.sort()
-    print(missingSolutionPartOrder)
 
     # crossed part of the solution
     for s in missingSolutionPartOrder:
         # append missing values to final crossover solution
         crossedSolutionOrder.append(s[1])
 
-    print("crossed soluttion part: ", crossedSolutionOrder)
+    return crossedSolutionOrder
 # 2. function for whole population
