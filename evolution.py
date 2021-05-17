c79aae9359210417dd1bc82815a61a55e9da522b
Waldemar <waldek99zielinski@gmail.com>
2021-05-17 17:59:39 +0200

    evolution draft

--- evolution.py
+++ evolution.py
@@ -1,1 +1,20 @@
 # For implementing evolutionary algorithm
+
+# evolution draft
+
+GENERATIONS = 100
+def evolutionary_algorithm(init_population, generations):
+
+    population = init_population
+
+    for _ in range(generations):
+        for x in population:
+            child = None
+            #TODO: think of a condition
+            if True:
+                # child = mutation X crossover X selection
+            else:
+                # child = mutation X selection
+            population.append(child)
+            
+            #population = replacement
