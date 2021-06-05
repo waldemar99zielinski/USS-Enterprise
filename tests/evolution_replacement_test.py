0611943dc37cdcb7463d882d09542314eadaac13
Waldemar <waldek99zielinski@gmail.com>
2021-06-05 20:57:33 +0200

    replacement test

---
+++ tests/evolution_replacement_test.py
@@ -0,0 +1,29 @@
+import unittest
+from solution_data import *
+from randomization import *
+from evolution_replacement import *
+
+
+class ReplacementTest(unittest.TestCase):
+
+    def test1(self):
+        initialize_rng(1255)
+
+        order1a = [5, 6, 1, 12, 7, 8, 15]
+        order1b = [15, 6, 12, 1, 8, 7, 5]
+        order1c = [12, 6, 15, 1, 5, 7, 8]
+        order1d = [1, 8, 12, 15, 7, 6, 5]
+        solution1a = SolutionInstance(order1a)
+        solution1b = SolutionInstance(order1b)
+        solution1c = SolutionInstance(order1c)
+        solution1d = SolutionInstance(order1d)
+        population = [solution1a, solution1b, solution1c, solution1d]
+
+        print(solution1a.fitness, solution1b.fitness, solution1c.fitness, solution1d.fitness)
+        print(most_fit(population, 2)[0].fitness, most_fit(population, 2)[1].fitness)
+
+
+
+
+
+
